731341b08513caf2bfbf544d70e5f48687af48aa
iTino7 <Sabatino.b007@outlook.it>
2025-11-12 13:02:18 +0100

    update controller

--- src/main/java/tino/portfolio/Controller/ContactController.java
+++ src/main/java/tino/portfolio/Controller/ContactController.java
@@ -2,11 +2,14 @@ package tino.portfolio.Controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tino.portfolio.dto.ContactRequest;
 import tino.portfolio.dto.ContactService;
 
-@CrossOrigin(origins = "http://localhost:5173")   // consente il frontend Vite
+
 @RestController
 @RequestMapping("/api/contact")
 public class ContactController {
